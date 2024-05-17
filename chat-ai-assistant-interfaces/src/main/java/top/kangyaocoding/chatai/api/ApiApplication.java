eda91a99ad2819951ea7820ef929c7c64fcceafe
K·Herbert <2294943022@qq.com>
2024-05-17 17:25:38 +0800

    feat: init

---
+++ chat-ai-assistant-interfaces/src/main/java/top/kangyaocoding/chatai/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package top.kangyaocoding.chatai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author K·Herbert
+ * @Description 启动入口
+ * @Date 2024-05-17 17:10
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chat-ai-assistant-interfaces/src/test/java/top/kangyaocoding/chatai/test/ApiTest.java
@@ -0,0 +1,10 @@
+package top.kangyaocoding.chatai.test;
+
+/**
+ * @Author K·Herbert
+ * @Description 单元测试
+ * @Date 2024-05-17 17:24
+ */
+
+public class ApiTest {
+}
